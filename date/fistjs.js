94cf46d5094e25e8dcbdb9e1b3ac76cd5bf782d2
khumaryn <119412655+khumaryn@users.noreply.github.com>
2023-02-14 01:16:05 +0400

    Add files via upload

---
+++ date/fistjs.js
@@ -0,0 +1,18 @@
+var tarix = new Date().getHours();
+if(tarix>=0 && tarix<=5){
+  alert('Good Night')  
+  document.body.style.backgroundImage="url('https://images.unsplash.com/photo-1568420162086-b16456b8d557?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHw%3D&w=1000&q=80')";
+}
+if(tarix>=6 && tarix<=11){
+    alert('Good Morning')  
+    document.body.style.backgroundImage="url('https://www.wishesmsg.com/wp-content/uploads/Good-Morning-Msg.jpg')";
+  }
+  if(tarix>=12 && tarix<=16){
+    alert('Good Afternoon')  
+    document.body.style.backgroundImage="url('https://blogger.googleusercontent.com/img/a/AVvXsEi43NZIl38jwEKAHpVqyQYOY9O2IJKBgp9IxCgGlDpy1J2qEribKTOYnqbMGPYyMSf81SapjCRKCqjP5n3Yo4uaPMThCOtbdf5O-3e3o4sSoZBc5XJbFZfdGw_sTG3FzzM6cVKMADzulFPWIiALrSebqtKB7a_xbxyMITtQQ4h--QvdTeErSaAQ_PVu=s16000')";
+  }
+  if(tarix>=17 && tarix<=23){
+    alert('Good Evening')  
+    document.body.style.backgroundImage="url('https://funkylife.in/wp-content/uploads/2022/08/good-evening-image-from-funkylife-26.jpg')";
+  }
+
